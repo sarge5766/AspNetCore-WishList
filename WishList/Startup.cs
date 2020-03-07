7a184f9598d2a69265e624b8dd9a6eb5c61ff75b
Scott Lampron <scott.lampron@gmail.com>
2020-03-07 05:28:02 -0500

    Completed Module

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -20,6 +20,7 @@ namespace WishList
             services.AddMvc();
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
